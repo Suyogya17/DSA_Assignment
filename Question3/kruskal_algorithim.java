351a64f9c605ad3f6a14afd01eb91da6f0f24fd5
Suyogya17 <suyogyashrestha17@gmail.com>
2024-02-19 22:55:06 +0545

    arra

--- Question3/kruskal_algorithim.java
+++ Question3/kruskal_algorithim.java
@@ -1,9 +1,7 @@
 package Question3;
 import java.util.*;
  
- 
-// Class to represent an edge with source, destination, and weight
-class Edge implements Comparable<Edge> {
+ class Edge implements Comparable<Edge> {
     int source, destination;
     double weight;
  
@@ -12,29 +10,20 @@ class Edge implements Comparable<Edge> {
         this.destination = destination;
         this.weight = weight;
     }
- 
-      // Comparing edges based on their weights
-    @Override
+     @Override
     public int compareTo(Edge other) {
         return Double.compare(this.weight, other.weight);
     }
 }
- 
- 
-//  implementing Kruskal's algorithm for finding Minimum Spanning Tree
-public class kruskal_algorithim {
+ public class kruskal_algorithim {
  
     private int vertices;
     private List<Edge> edges;
- 
-    // Constructor to initialize the graph with a given number of vertices
-    public kruskal_algorithim(int vertices) {
+     public kruskal_algorithim(int vertices) {
         this.vertices = vertices;
         this.edges = new ArrayList<>();
     }
- 
-      // Method to add an edge to the graph
-    public void addEdge(int source, int destination, double weight) {
+     public void addEdge(int source, int destination, double weight) {
         edges.add(new Edge(source, destination, weight));
     }
  
@@ -45,17 +34,15 @@ public class kruskal_algorithim {
         return find(parent, parent[i]);
     }
  
-     // Method to perform union of two sets
     private void union(int[] parent, int x, int y) {
         int rootX = find(parent, x);
         int rootY = find(parent, y);
         parent[rootX] = rootY;
     }
  
-     // Kruskal's algorithm to find Minimum Spanning Tree
     public List<Edge> kruskalMST() {
         List<Edge> result = new ArrayList<>();
-        Collections.sort(edges); // Sort edges based on weights
+        Collections.sort(edges); 
  
         int[] parent = new int[vertices];
         for (int i = 0; i < vertices; i++) {
@@ -65,9 +52,7 @@ public class kruskal_algorithim {
         for (Edge edge : edges) {
             int rootSource = find(parent, edge.source);
             int rootDestination = find(parent, edge.destination);
- 
-             // If including this edge doesn't create a cycle, add it to the result
-            if (rootSource != rootDestination) {
+             if (rootSource != rootDestination) {
                 result.add(edge);
                 union(parent, rootSource, rootDestination);
             }
